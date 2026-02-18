4aaa7e35b5a1f660a26f71df7a00442ce2ff6647
Eakawat Buanitisin <aekawat.boom@gmail.com>
2026-02-18 12:23:51 +0700

    added some elements

---
+++ js/data.js
@@ -0,0 +1,29 @@
+// /js/data.js
+const facultyData = {
+    "amnat": {
+        category: "iot",
+        name: "ผศ. ดร. อำนาจ ขาวเน",
+        fullName: "ผศ. ดร. อำนาจ ขาวเน (Asst.Prof.Dr. Amnach Khawne)",
+        position: "อาจารย์",
+        email: "amnach.kh@kmitl.ac.th",
+        room: "ECC 914",
+        phone: "02-7392400~2 ต่อ 126",
+        image: "/images/amnach-large.png",
+        bachelor: "วศ.บ. (สถาบันเทคโนโลยีพระจอมเกล้าเจ้าคุณทหารลาดกระบัง)",
+        master: "วศ.ม. (สถาบันเทคโนโลยีพระจอมเกล้าเจ้าคุณทหารลาดกระบัง)",
+        doctor: "Ph.D. (Electrical Engineering)"
+    },
+    "eakawat": {
+        category: "iot",
+        name: "ดร. บูม", 
+        fullName: "ดร. สมชาย ใจดี (Dr. Somchai Jaidee)",
+        position: "อาจารย์",
+        email: "eakawat.j@kmitl.ac.th",
+        room: "E12",
+        phone: "096-361-6549",
+        image: "/images/amnach-large.png", // Replace with actual image path later
+        bachelor: "B.Eng. (Computer Engineering)",
+        master: "M.Eng. (Information Technology)",
+        doctor: "D.Eng. (IoT Systems)"
+    }
+};
